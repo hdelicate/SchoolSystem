fa93c7566aa8eea642d5f3a820b9abe43a103792
hdelicate <46897871+hdelicate@users.noreply.github.com>
2020-02-03 22:57:57 -0600

    Add project files.

---
+++ SchoolSystem/Home.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSystem
+{
+    class Home
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter enter a 1) Student, 2) Teacher, 3) End:");
+          var personRole=  Console.ReadLine();
+            List<Person> People = new List<Person>();
+            while (!personRole.Contains("3"))
+            {
+                if (personRole == "1")
+                {
+                    var p = new Student();
+
+                    Console.WriteLine("Name: ");
+                    p.Name = Console.ReadLine();
+                    Console.WriteLine("Age: ");
+                    p.Age = Console.ReadLine();
+                    Console.WriteLine("Id: ");
+                    p.id = Console.ReadLine();
+                    Console.WriteLine("Program: [1) Computer Science, 2) Accounting, 3) Marketing, 4)Nursing, 5)End ");
+                    int program1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Credits Earned: ");
+                    p.creditsEarned = Console.ReadLine();
+                    p.program = (Program)program1;
+                    People.Add(p);
+                }
+                if (personRole == "2")
+                {
+                    var p = new teacher();
+                    Console.WriteLine("Name: ");
+                    p.Name = Console.ReadLine();
+                    Console.WriteLine("Age: ");
+                    p.Age = Console.ReadLine();
+                    Console.WriteLine("Id: ");
+                    p.id = Console.ReadLine();
+                    Console.WriteLine("Program: [1) Computer Science, 2) Accounting, 3) Marketing, 4)Nursing, 5)End ");
+                    int program1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Years of Service:  ");
+                    p.yearsOfService = Console.ReadLine();
+                    p.program = (Program)program1;
+                    People.Add(p);
+                }
+                Console.WriteLine("Please enter enter a 1) Student, 2) Teacher, 3) End:");
+                personRole = Console.ReadLine();
+            }
+            
+                for (int i = 0; i < People.Count; i++)
+                {
+                    Console.WriteLine(People[i].ToString());
+                }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SchoolSystem/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSystem
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string id { get; set; }
+        public Program program;
+        public override string ToString()
+        {
+            string s = "";
+            switch ((int)program) {
+                case 1: s = "ComputerScience";
+                    break;
+                case 2:
+                    s = "Accounting";
+                    break;
+                case 3:
+                    s = "Marketing";
+                    break;
+                case 4:
+                    s = "Nursing";
+                    break;
+
+            }
+            return "Name: " + Name + " Age: " + " Id: " + id + " program: " + s;
+        }
+
+    }
+    public enum Program { ComputerScience =1, Accounting=2, Marketing=3, Nursing=4}
+}
